6bc2c471e26ec47395cb42aec3c647f3fdb0dc23
Robert Burger <robert.burger@dlr.de>
2020-01-30 14:05:36 +0100

    added rk_gui_plugin

---
+++ bindings/python/rk_gui_plugin/service_provider_canopen_protocol/canopen_emergency_treeview.py
@@ -0,0 +1,229 @@
+'''
+(C) Robert Burger <robert.burger@dlr.de>
+
+This file is part of Robotkernel-GUI.
+
+Robotkernel-GUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Robotkernel-GUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Robotkernel-GUI.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import gtk
+import gobject
+import helpers
+
+import helpers
+from canopen_types import *
+from helpers.treestore_helpers import search, match_func
+
+class canopen_emergency_treeview(object):
+    def __init__(self):
+        pass
+
+    def create_treeview(self, view):
+        self.treestore_dictionary = store = gtk.TreeStore(str, gobject.TYPE_PYOBJECT) # soem id type
+        view = self.treeview_dictionary = view
+        view.set_model(store)
+        view.set_border_width(4)
+        view.set_headers_visible(True)
+
+        cell_colors = { True: self.active_color, False: "grey" }
+
+        # ------------------ index column ------------------
+        def cb_idn(column, cell, store, iter):
+            cell.set_property("xalign", 1.0)
+            if store.iter_parent(iter):
+                cell.set_property('text', '%d'  % store[iter][0])
+            else:
+                cell.set_property('text', '0x%04X'  % store[iter][0])
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "IDn" , gtk.CellRendererText(), cb_idn)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
+
+        # ------------------ name column -------------------
+        def cb_name(column, cell, store, iter):
+            parent_iter = store.iter_parent(iter)
+            if not helpers.treestore_helpers.is_row_visible(store, iter, self.treeview_dictionary):
+                return
+
+            row = store[iter]
+            device = row[2]
+
+            if parent_iter:
+                index        = store[parent_iter][0]
+                sub_index    = row[0]
+                object       = device.canopen_dictionary[index]
+                element      = object.subindices[sub_index]
+                element.get_data()
+                cell.set_property("text", element.name)
+            else:
+                index        = row[0]
+                object       = device.canopen_dictionary[index]
+                object.get_data()
+
+                if object.objcode == 8 or object.objcode == 9:
+                    for sub in range(0, object.max_subindices + 1):
+                        sub_iter = search([row, ], match_func, ([0, sub], ))
+                        if sub_iter is None:
+                            self.treestore_dictionary.insert(iter, -1, [sub, "", device])
+
+                cell.set_property("text", object.name)
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Name", gtk.CellRendererText(), cb_name)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+        column.set_expand(True)
+        column.set_resizable(True)
+
+        def cb_datatype(column, cell, store, iter):
+            parent_iter = store.iter_parent(iter)
+            if not helpers.treestore_helpers.is_row_visible(store, iter, self.treeview_dictionary):
+                return
+
+            row = store[iter]
+            device = row[2]
+
+            if parent_iter:
+                index        = store[parent_iter][0]
+                sub_index    = row[0]
+                object       = device.canopen_dictionary[index]
+                element      = object.subindices[sub_index]
+                element.get_data()
+                if element.data_type in canopen_datatypes:
+                    cell.set_property("text", canopen_datatypes[element.data_type])
+                else:
+                    cell.set_property("text", "unknown 0x%X" % element.data_type)
+            else:
+                index        = row[0]
+                object       = device.canopen_dictionary[index]
+                object.get_data()
+
+                if object.objcode == 8 or object.objcode == 9:
+                    for sub in range(0, object.max_subindices + 1):
+                        sub_iter = search([row, ], match_func, ([0, sub], ))
+                        if sub_iter is None:
+                            self.treestore_dictionary.insert(iter, -1, [sub, "", device])
+
+                if object.data_type in canopen_datatypes:
+                    cell.set_property("text", canopen_datatypes[object.data_type])
+                else:
+                    cell.set_property("text", "unknown 0x%X" % object.data_type)
+
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Datatype", gtk.CellRendererText(), cb_datatype)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+        column.set_resizable(True)
+
+        def cb_objcode(column, cell, store, iter):
+            parent_iter = store.iter_parent(iter)
+            if not helpers.treestore_helpers.is_row_visible(store, iter, self.treeview_dictionary):
+                return
+
+            row = store[iter]
+            device = row[2]
+
+            if parent_iter:
+                index        = store[parent_iter][0]
+                sub_index    = row[0]
+                object       = device.canopen_dictionary[index]
+                element      = object.subindices[sub_index]
+                element.get_data()
+                if element.objcode in canopen_objcodes:
+                    cell.set_property("text", canopen_objcodes[element.objcode])
+                else:
+                    cell.set_property("text", "unknown 0x%X" % element.objcode)
+            else:
+                index        = row[0]
+                object       = device.canopen_dictionary[index]
+                object.get_data()
+
+                if object.objcode == 8 or object.objcode == 9:
+                    for sub in range(0, object.max_subindices + 1):
+                        sub_iter = search([row, ], match_func, ([0, sub], ))
+                        if sub_iter is None:
+                            self.treestore_dictionary.insert(iter, -1, [sub, "", device])
+
+                if object.objcode in canopen_objcodes:
+                    cell.set_property("text", canopen_objcodes[object.objcode])
+                else:
+                    cell.set_property("text", "unknown 0x%X" % object.objcode)
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Object code", gtk.CellRendererText(), cb_objcode)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+        column.set_resizable(True)
+
+        # ------------------ data column -------------------
+        def cb_data(column, cell, store, iter):
+            parent_iter = store.iter_parent(iter)
+            if not helpers.treestore_helpers.is_row_visible(store, iter, self.treeview_dictionary):
+                return
+
+            row = store[iter]
+            device = row[2]
+
+            if parent_iter:
+                index        = store[parent_iter][0]
+                sub_index    = row[0]
+                object       = device.canopen_dictionary[index]
+                element      = object.subindices[sub_index]
+                element.get_data()
+                cell.set_property("text", element.value)
+                cell.set_property("foreground", cell_colors[element.valid])
+            else:
+                index        = row[0]
+                object       = device.canopen_dictionary[index]
+                object.get_data()
+                if object.objcode == 7:
+                    element      = object.subindices[0]
+                    element.get_data()
+                    cell.set_property("text", element.value)
+                else:
+                    cell.set_property("text", "")
+
+            return True
+
+        cell_renderer = gtk.CellRendererText()
+        cell_renderer.set_property("editable", True)
+        cell_renderer.connect("edited", self.edit_canopen_value)
+
+        col_cnt = view.insert_column_with_data_func(-1, "Value", cell_renderer, cb_data)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
+        column.set_expand(True)
+        column.set_resizable(True)
+        store.set_sort_column_id(0, 0)
+
+        return (view, store)
+
+    def edit_canopen_value(self, cr, path, newvalue):
+        store = self.treestore_dictionary
+        iter = store.get_iter(path)
+        parent_iter = store.iter_parent(iter)
+
+        if parent_iter:
+            idx = store[parent_iter][0]
+            device = store[parent_iter][2]
+            sub_idx = store[iter][0]
+            device.write_element(idx, sub_idx, newvalue)
+        else:
+            idx = store[iter][0]
+            device = store[iter][2]
+            device.write_element(idx, 0, newvalue)
+
+        return True
---
+++ bindings/python/rk_gui_plugin/service_provider_canopen_protocol/canopen_protocol_view.py
@@ -0,0 +1,139 @@
+'''
+(C) Robert Burger <robert.burger@dlr.de>
+
+This file is part of Robotkernel-GUI.
+
+Robotkernel-GUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Robotkernel-GUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Robotkernel-GUI.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+import gtk
+import gobject
+
+from canopen_treeview import canopen_treeview
+from canopen_device import canopen_device
+
+import helpers
+
+class canopen_protocol_view(helpers.service_provider_view, helpers.builder_base, canopen_treeview):
+    def __init__(self, parent, container):
+        fn = os.path.join(os.path.dirname(__file__), 'canopen_protocol_view.ui')
+        helpers.builder_base.__init__(self, fn, 'canopen_protocol_box')
+        helpers.service_provider_view.__init__(self, parent.app, parent, self.canopen_protocol_box, 'object_dictionary_list')
+
+        container.pack_start(self.canopen_protocol_box, True, True)
+
+        self.devices = {}
+        self.current_device = None
+
+        self.canopen_protocol_emergencies_store = store = gtk.ListStore(gobject.TYPE_PYOBJECT) # soem id type
+        view = self.canopen_protocol_emergencies_tv
+        view.set_model(store)
+        view.set_border_width(4)
+        view.set_headers_visible(True)
+
+        # ------------------ index column ------------------
+        def cb_datetime(column, cell, store, iter):
+            row = store[iter]
+            emergency = row[0]
+            cell.set_property("xalign", 0.0)
+            cell.set_property('text', str(row[0].timestamp))
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Datetime" , gtk.CellRendererText(), cb_datetime)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
+
+        # ------------------ error_code column ------------------
+        def cb_error_code(column, cell, store, iter):
+            row = store[iter]
+            emergency = row[0]
+            cell.set_property("xalign", 0.0)
+            cell.set_property('text', "0x%04X" % (row[0].error_code))
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Error-Code" , gtk.CellRendererText(), cb_error_code)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
+
+        # ------------------ error_register column ------------------
+        def cb_error_register(column, cell, store, iter):
+            row = store[iter]
+            cell.set_property("xalign", 0.0)
+            cell.set_property('text', "0x%04X" % (row[0].error_register))
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Error-Register" , gtk.CellRendererText(), cb_error_register)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
+
+        # ------------------ data column ------------------
+        def cb_data(column, cell, store, iter):
+            row = store[iter]
+            cell.set_property("xalign", 0.0)
+            cell.set_property('text', ', '.join(["%02X" % x for x in row[0].data]))
+            return True
+
+        col_cnt = view.insert_column_with_data_func(-1, "Data" , gtk.CellRendererText(), cb_data)
+        column  = view.get_column(col_cnt - 1)
+        column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
+
+        #self.canopen_protocol_vpaned.set_position(500)
+        #setattr(self.canopen_protocol_vpaned.get_child1(), "resize", True)
+        #setattr(self.canopen_protocol_vpaned.get_child2(), "resize", False)
+        self.canopen_protocol_refresh_btn.connect("clicked", self.on_refresh)
+        self.active_color = self.app.window.get_style().text[0].to_string()
+        self.create_treeview(self.canopen_protocol_tv)
+        self.tv = self.treeview_dictionary
+
+    def on_canopen_protocol_refresh_emergencies_btn_clicked(self, btn):
+        if self.current_device is None:
+            return
+
+        while True:
+            e = self.current_device.upload_emergencies()
+            if not e:
+                break;
+
+            itr = self.canopen_protocol_emergencies_store.append([e, ])
+
+    def on_refresh(self, btn):
+        if self.current_device is None:
+            return
+
+        for index in self.current_device.canopen_dictionary:
+            for sub_index in self.current_device.canopen_dictionary[index].subindices:
+                self.current_device.canopen_dictionary[index].subindices[sub_index].valid = False
+
+        self.treeview_dictionary.queue_draw()
+
+    def show(self, modname, devname):
+        #if self.current_device and self.current_device.modname == modname and self.current_device.devname == devname:
+        #    return
+
+        modidx = (modname, devname)
+        if modidx not in self.devices:
+            self.devices[modidx] = canopen_device(self.service_prefix, self.app, self.treeview_dictionary, modname, devname)
+
+        self.current_device = self.devices[modidx]
+        helpers.service_provider_view.show(self)
+        self.show_indices()
+
+    def show_indices(self):
+        self.treestore_dictionary.clear()
+        self.current_device.list_dictionary()
+
+        map(lambda x: self.treestore_dictionary.insert(None, -1, [x, "", self.current_device]),
+                self.current_device.canopen_dictionary)
+
