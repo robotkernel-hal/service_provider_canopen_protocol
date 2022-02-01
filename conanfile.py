b6c314fb58f6e5e137ae7959ed450e699f7ce26d
Robert Burger <robert.burger@dlr.de>
2022-02-01 06:59:11 +0100

    ADD: build of ln message definitions

--- conanfile.py
+++ conanfile.py
@@ -2,16 +2,20 @@ from conans import ConanFile, tools
 import os
 
 class MainProject(ConanFile):
-    python_requires = "conan_template_ln_generator/[~=5 >=5.0.7]@robotkernel/stable"
-    python_requires_extend = "conan_template_ln_generator.RobotkernelLNGeneratorConanFile"
+    python_requires = "conan_template/[~=5]@robotkernel/stable"
+    python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "service_provider_canopen_protocol"
     description = "robotkernel service provider for canopen protocol devices."
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-    requires = "robotkernel/[~=5]@robotkernel/stable"
+
+    def requirements(self):
+        self.requires(f"{self.name}_ln_msgdef/{self.version}@{self.user}/{self.channel}")
+        self.requires("robotkernel_service_helper/[*]@robotkernel/stable")
+        self.requires("robotkernel/[~=5]@robotkernel/stable")
 
     def package_info(self):
-        base = self.python_requires["conan_template_ln_generator"].module.RobotkernelLNGeneratorConanFile
+        base = self.python_requires["conan_template"].module.RobotkernelLNGeneratorConanFile
         base.package_info(self)
 
         self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "bindings/python"))
